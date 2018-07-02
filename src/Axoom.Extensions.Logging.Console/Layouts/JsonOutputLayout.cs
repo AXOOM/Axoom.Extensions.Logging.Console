02c0c25eea5273f91c1d8dfb548019d06adee5bf
Jens Kinner <jens.kinner@axoom.com>
2018-07-02 08:08:07 +0200

    Renames level to syslog_level

--- src/Axoom.Extensions.Logging.Console/Layouts/JsonOutputLayout.cs
+++ src/Axoom.Extensions.Logging.Console/Layouts/JsonOutputLayout.cs
@@ -19,10 +19,10 @@ namespace Axoom.Extensions.Logging.Console.Layouts
             SuppressSpaces = true;
 
             Attributes.Add(new JsonAttribute("message", LayoutFormats.MESSAGE));
-            Attributes.Add(new JsonAttribute("level", LayoutFormats.SYS_LOGLEVEL, encode: false));
             Attributes.Add(new JsonAttribute("timestamp_unix", LayoutFormats.TIMESTAMP_UNIX, encode: false));
             Attributes.Add(new JsonAttribute("timestamp_iso8601", LayoutFormats.TIMESTAMP_ISO8601));
             Attributes.Add(new JsonAttribute("loglevel", LayoutFormats.LOGLEVEL));
+            Attributes.Add(new JsonAttribute("syslog_level", LayoutFormats.SYS_LOGLEVEL, encode: false));
             Attributes.Add(new JsonAttribute("logger", LayoutFormats.LOGGER));
             Attributes.Add(new JsonAttribute("callsite", LayoutFormats.CALLSITE));
             Attributes.Add(new JsonAttribute("exception_type", LayoutFormats.EXCEPTION_TYPE));
